1950d192e42a43376e2bc53ba41922b1677f8c62
Woshiajuana <zhigang.chen@owulia.com>
2017-05-03 18:55:50 +0800

    账单页面菜单栏

--- src/views/bill/Bill.vue
+++ src/views/bill/Bill.vue
@@ -1,145 +1,163 @@
 <template>
     <div class="container-view">
-        <h1 class="header-title">账单：</h1>
-        <svg slot="icon" class="bill-filter">
-            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#filter-icon"></use>
-        </svg>
-        <div class="bill-wrap">
-            <scroller lock-x
-                      height="-118"
-                      :scrollbarY="true"
-                      @on-scroll="onScroll"
-                      :use-pulldown="true"
-                      :use-pullup="true"
-                      @on-pulldown-loading="onPullDownLoading"
-                      @on-pullup-loading="onPullUpLoading"
-                      :pulldown-config="pull_down_config"
-                      :pullup-config="pull_up_config"
-                      ref="homeScrollEvent">
-                <ul class="bill-list">
-                    <li class="bill-item">
+        <div class="container-box"
+            :class="{'open-menu': is_open_menu}">
+            <h1 class="header-title">账单：</h1>
+            <svg @click="is_open_menu=!is_open_menu" slot="icon" class="bill-filter">
+                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#filter-icon"></use>
+            </svg>
+            <div class="filter-menu">
+                <a href="#/" class="filter-menu-item">水果零食</a>
+                <a href="#/" class="filter-menu-item">餐饮伙食</a>
+                <a href="#/" class="filter-menu-item">出行旅游</a>
+                <a href="#/" class="filter-menu-item">出行旅游</a>
+                <a href="#/" class="filter-menu-item">出行旅游</a>
+                <a href="#/" class="filter-menu-item">出行旅游</a>
+                <a href="#/" class="filter-menu-item">出行旅游</a>
+                <a href="#/" class="filter-menu-item">出行旅游</a>
+                <a href="#/" class="filter-menu-item">出行旅游</a>
+                <a href="#/" class="filter-menu-item">出行旅游</a>
+                <a href="#/" class="filter-menu-item">出行旅游</a>
+                <a href="#/" class="filter-menu-item">出行旅游</a>
+                <a href="#/" class="filter-menu-item">出行旅游</a>
+            </div>
+            <div class="bill-wrap">
+                <scroller lock-x
+                          height="-118"
+                          :scrollbarY="true"
+                          @on-scroll="onScroll"
+                          :use-pulldown="true"
+                          :use-pullup="true"
+                          @on-pulldown-loading="onPullDownLoading"
+                          @on-pullup-loading="onPullUpLoading"
+                          :pulldown-config="pull_down_config"
+                          :pullup-config="pull_up_config"
+                          ref="homeScrollEvent">
+                    <ul class="bill-list">
+                        <li class="bill-item">
                         <span class="bill-item-type consumption-type">
                             <svg slot="icon" class="bill-item-type-icon">
                                 <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#type-sgls"></use>
                             </svg>
                         </span>
-                        <p class="bill-item-con">
-                            <span class="bill-item-remark">水果零食</span>
-                            <span class="bill-item-sum">50.00</span>
-                        </p>
-                        <p class="bill-item-time">消费时间：2017-05-03 17：18</p>
-                    </li>
-                    <li class="bill-item">
+                            <p class="bill-item-con">
+                                <span class="bill-item-remark">水果零食</span>
+                                <span class="bill-item-sum">50.00</span>
+                            </p>
+                            <p class="bill-item-time">消费时间：2017-05-03 17：18</p>
+                        </li>
+                        <li class="bill-item">
                         <span class="bill-item-type earn-type">
                             <svg slot="icon" class="bill-item-type-icon">
                                 <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#type-cshs"></use>
                             </svg>
                         </span>
-                        <p class="bill-item-con">
-                            <span class="bill-item-remark">餐饮伙食</span>
-                            <span class="bill-item-sum">50.00</span>
-                        </p>
-                        <p class="bill-item-time">消费时间：2017-05-03 17：18</p>
-                    </li>
-                    <li class="bill-item">
+                            <p class="bill-item-con">
+                                <span class="bill-item-remark">餐饮伙食</span>
+                                <span class="bill-item-sum">50.00</span>
+                            </p>
+                            <p class="bill-item-time">消费时间：2017-05-03 17：18</p>
+                        </li>
+                        <li class="bill-item">
                         <span class="bill-item-type earn-type">
                             <svg slot="icon" class="bill-item-type-icon">
                                 <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#type-wsgw"></use>
                             </svg>
                         </span>
-                        <p class="bill-item-con">
-                            <span class="bill-item-remark">网上购物</span>
-                            <span class="bill-item-sum">50.00</span>
-                        </p>
-                        <p class="bill-item-time">消费时间：2017-05-03 17：18</p>
-                    </li>
-                    <li class="bill-item">
+                            <p class="bill-item-con">
+                                <span class="bill-item-remark">网上购物</span>
+                                <span class="bill-item-sum">50.00</span>
+                            </p>
+                            <p class="bill-item-time">消费时间：2017-05-03 17：18</p>
+                        </li>
+                        <li class="bill-item">
                         <span class="bill-item-type earn-type">
                             <svg slot="icon" class="bill-item-type-icon">
                                 <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#type-shrc"></use>
                             </svg>
                         </span>
-                        <p class="bill-item-con">
-                            <span class="bill-item-remark">生活日常</span>
-                            <span class="bill-item-sum">50.00</span>
-                        </p>
-                        <p class="bill-item-time">消费时间：2017-05-03 17：18</p>
-                    </li>
-                    <li class="bill-item">
+                            <p class="bill-item-con">
+                                <span class="bill-item-remark">生活日常</span>
+                                <span class="bill-item-sum">50.00</span>
+                            </p>
+                            <p class="bill-item-time">消费时间：2017-05-03 17：18</p>
+                        </li>
+                        <li class="bill-item">
                         <span class="bill-item-type earn-type">
                             <svg slot="icon" class="bill-item-type-icon">
                                 <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#type-cfsd"></use>
                             </svg>
                         </span>
-                        <p class="bill-item-con">
-                            <span class="bill-item-remark">租房水电</span>
-                            <span class="bill-item-sum">50.00</span>
-                        </p>
-                        <p class="bill-item-time">消费时间：2017-05-03 17：18</p>
-                    </li>
-                    <li class="bill-item">
+                            <p class="bill-item-con">
+                                <span class="bill-item-remark">租房水电</span>
+                                <span class="bill-item-sum">50.00</span>
+                            </p>
+                            <p class="bill-item-time">消费时间：2017-05-03 17：18</p>
+                        </li>
+                        <li class="bill-item">
                         <span class="bill-item-type earn-type">
                             <svg slot="icon" class="bill-item-type-icon">
                                 <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#type-ylyw"></use>
                             </svg>
                         </span>
-                        <p class="bill-item-con">
-                            <span class="bill-item-remark">医疗药物</span>
-                            <span class="bill-item-sum">50.00</span>
-                        </p>
-                        <p class="bill-item-time">消费时间：2017-05-03 17：18</p>
-                    </li>
-                    <li class="bill-item">
+                            <p class="bill-item-con">
+                                <span class="bill-item-remark">医疗药物</span>
+                                <span class="bill-item-sum">50.00</span>
+                            </p>
+                            <p class="bill-item-time">消费时间：2017-05-03 17：18</p>
+                        </li>
+                        <li class="bill-item">
                         <span class="bill-item-type earn-type">
                             <svg slot="icon" class="bill-item-type-icon">
                                 <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#type-qt"></use>
                             </svg>
                         </span>
-                        <p class="bill-item-con">
-                            <span class="bill-item-remark">其它消费</span>
-                            <span class="bill-item-sum">50.00</span>
-                        </p>
-                        <p class="bill-item-time">消费时间：2017-05-03 17：18</p>
-                    </li>
+                            <p class="bill-item-con">
+                                <span class="bill-item-remark">其它消费</span>
+                                <span class="bill-item-sum">50.00</span>
+                            </p>
+                            <p class="bill-item-time">消费时间：2017-05-03 17：18</p>
+                        </li>
 
-                    <li class="bill-item">
+                        <li class="bill-item">
                         <span class="bill-item-type earn-type">
                             <svg slot="icon" class="bill-item-type-icon">
                                 <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#type-jbgz"></use>
                             </svg>
                         </span>
-                        <p class="bill-item-con">
-                            <span class="bill-item-remark">基本工资</span>
-                            <span class="bill-item-sum">50.00</span>
-                        </p>
-                        <p class="bill-item-time">入账时间：2017-05-03 17：18</p>
-                    </li>
-                    <li class="bill-item">
+                            <p class="bill-item-con">
+                                <span class="bill-item-remark">基本工资</span>
+                                <span class="bill-item-sum">50.00</span>
+                            </p>
+                            <p class="bill-item-time">入账时间：2017-05-03 17：18</p>
+                        </li>
+                        <li class="bill-item">
                         <span class="bill-item-type earn-type">
                             <svg slot="icon" class="bill-item-type-icon">
                                 <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#type-gsfl"></use>
                             </svg>
                         </span>
-                        <p class="bill-item-con">
-                            <span class="bill-item-remark">公司福利</span>
-                            <span class="bill-item-sum">50.00</span>
-                        </p>
-                        <p class="bill-item-time">入账时间：2017-05-03 17：18</p>
-                    </li>
-                    <li class="bill-item">
+                            <p class="bill-item-con">
+                                <span class="bill-item-remark">公司福利</span>
+                                <span class="bill-item-sum">50.00</span>
+                            </p>
+                            <p class="bill-item-time">入账时间：2017-05-03 17：18</p>
+                        </li>
+                        <li class="bill-item">
                         <span class="bill-item-type earn-type">
                             <svg slot="icon" class="bill-item-type-icon">
                                 <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#type-qt"></use>
                             </svg>
                         </span>
-                        <p class="bill-item-con">
-                            <span class="bill-item-remark">其它入账</span>
-                            <span class="bill-item-sum">50.00</span>
-                        </p>
-                        <p class="bill-item-time">入账时间：2017-05-03 17：18</p>
-                    </li>
-                </ul>
-            </scroller>
+                            <p class="bill-item-con">
+                                <span class="bill-item-remark">其它入账</span>
+                                <span class="bill-item-sum">50.00</span>
+                            </p>
+                            <p class="bill-item-time">入账时间：2017-05-03 17：18</p>
+                        </li>
+                    </ul>
+                </scroller>
+            </div>
         </div>
     </div>
 </template>
@@ -150,6 +168,7 @@
         name: 'bill',
         data () {
             return {
+                is_open_menu: false,
                 date_value: '',
                 pull_down_config: {
                     content: '下拉刷新',
@@ -215,6 +234,32 @@
         width: 20px;
         height: 20px;
         fill: #999;
+    }
+    .filter-menu-item{
+        @extend %db;
+        @extend %f12;
+        @extend %cp;
+        padding-left: 10px;
+        line-height: 30px;
+    }
+    .container-box{
+        @extend %h100;
+        transition: all .5s;
+        &.open-menu{
+            transform: translate3d(-80%,0,0);
+        }
+    }
+    .filter-menu{
+        @extend %pa;
+        @extend %t0;
+        @extend %r0;
+        @extend %b0;
+        transition: all .5s;
+        transform: translate3d(100%,0,0);
+        width: 90%;
+        z-index: 1;
+        border-left: 1px solid #ddd;
+        background-color: #fff;
     }
     .bill-list{
         margin: 0 10px 20px;
